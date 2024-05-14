3063cd25e2f08feaa3f7763f0ad8197b16f869e7
Xin shen <363088847@qq.com>
2024-05-14 15:36:37 +0800

    改为 alarms

--- background.ts
+++ background.ts
@@ -1,58 +1,18 @@
-import { AUTO_CLOSE_TAB_RULES } from "./const";
 import { storageConfig } from "~store";
-
-const tabTimes: Record<number, TabInfo> = {};
-let currentTabId: number | null = null;
+import { AUTO_CLOSE_TAB_RULES } from "./const";
+console.log("background started")
 let rules: RuleType[];
-const updateCurrentTabs = () => {
-  chrome.tabs.query({}, (tabs) => {
-    const now = Date.now(); // 获取当前时间
-    tabs.forEach((tab) => {
-      // 对每个tab进行处理
-      if (tab.id) {
-        const domain = new URL(tab.url || '').hostname; // 安全地读取URL，考虑到可能tab.url为空的情况
-        tabTimes[tab.id] = { lastVisited: now, domain }; // 更新或设置tabTimes中的信息
-      }
-    });
-    if (tabs.find(tab => tab.active)) {
-      // 如果存在活动标签，则更新currentTabId
-      currentTabId = tabs.find(tab => tab.active)?.id;
-    }
-  });
-}
-const main = async () => {
-  const data = await storageConfig.instance.get(storageConfig.key) // "value"
-  rules = data as any;
-  updateCurrentTabs()
-}
+let currentTab: chrome.tabs.Tab;
 
-main()
-
-chrome.tabs.onActivated.addListener((activeInfo) => {
-  currentTabId = activeInfo.tabId;
-  const now = Date.now();
-  tabTimes[currentTabId] = { ...tabTimes[currentTabId], lastVisited: now };
-
-  for (const tabIdStr in tabTimes) {
-    const tabId = Number(tabIdStr);
-    if (tabId !== currentTabId) {
-      tabTimes[tabId] = { ...tabTimes[tabId], lastVisited: now };
-    }
-  }
-});
-
-chrome.tabs.onRemoved.addListener((tabId) => {
-  delete tabTimes[tabId];
-});
 function matchDomain(rule: RuleType, domain: string): boolean {
   let matchExpression = rule.match;
-  console.log('domain', domain)
-  console.log('matchExpression', matchExpression)
+  // console.log('domain', domain)
+  // console.log('matchExpression', matchExpression)
   // 如果使用正则表达式，则其他规则失效
   if (rule.matchType.includes('regular')) {
     try {
       let regExp = new RegExp(matchExpression);
-      console.log('正则匹配的结果是', regExp.test(domain))
+      // console.log('正则匹配的结果是', regExp.test(domain))
       return regExp.test(domain);
     } catch (e) {
       console.error('Invalid regular expression:', matchExpression);
@@ -62,65 +22,99 @@ function matchDomain(rule: RuleType, domain: string): boolean {
 
   // 如果不区分大小写，则统一转换为小写
   if (!rule.matchType.includes('case')) {
-    console.log('不区分大小写')
+    // console.log('不区分大小写')
     matchExpression = matchExpression.toLowerCase();
     domain = domain.toLowerCase();
   }
-  console.log('matchExpression', matchExpression)
-  console.log('domain', domain)
+  // console.log('matchExpression', matchExpression)
+  // console.log('domain', domain)
   let matchResult = domain.includes(matchExpression);
 
   // 如果要匹配整个单词
   if (rule.matchType.includes('wholeWord')) {
-    console.log('=======')
-    console.log('rule.match', rule.match)
-    console.log('domain', domain)
-    console.log('rule.match === domain', rule.match === domain)
+    // console.log('=======')
+    // console.log('rule.match', rule.match)
+    // console.log('domain', domain)
+    // console.log('rule.match === domain', rule.match === domain)
     matchResult = rule.match === domain;
   }
 
   return matchResult;
 }
-function checkForIdleTabs() {
-  if (!rules || rules.length === 0) return;
-  console.log('===========开始检查==========');
-  const now = Date.now();
+async function createAlarmForTab(tab: chrome.tabs.Tab) {
+  console.log('createAlarmForTab', tab)
+  if (!tab.url || !tab.id) {
+    return;
+  }
+  // const domain = new URL(tab.url).hostname;
+  const rule = rules.find((r) => matchDomain(r, tab.url));
+
+  if (rule) {
+    // console.log('匹配成功',)
+    // console.log('rule', rule)
+    // console.log('tab.url', tab.url)
+    await chrome.alarms.create(String(tab.id), { delayInMinutes: 0.5 });
+    console.log('chrome.alarms.getAll()', await chrome.alarms.getAll())
+  }
+}
+function closeTab(alarm: { name: string; }) {
+  const tabId = Number(alarm.name);
   chrome.tabs.query({}, (tabs) => {
-    tabs.forEach((tab) => {
-      const tabId = tab.id;
-      const url = tab.url;
-      const rule = rules.find(rule => matchDomain(rule, url));
-      if (!rule) {
-        console.log('rule,匹配失败')
-        return;
-      }
-      console.log('rule,匹配成功', rule, tabId)
-      const tabInfo = tabTimes[tabId];
-      console.log('tabInfo', tabInfo)
-      const diffTime = now - tabInfo?.lastVisited
-      console.log('diffTime', diffTime)
-      console.log('rule.time', rule.time)
-      const configTimeout = Number(rule.time)
-      console.log('configTimeout', configTimeout)
-      if (tabId !== currentTabId && diffTime > configTimeout) {
-        chrome.tabs.remove(tabId, () => {
-          console.log(`Closed tab id ${tabId} due to inactivity.`);
-          delete tabTimes[tabId];
-        });
-      }
-    });
+    if (tabs.find(i => i.id === tabId)) {
+      chrome.tabs.remove(tabId, () => {
+        console.log(`Closed tab id ${tabId} due to inactivity.`);
+      });
+    }
   });
+
+}
+const main = async () => {
+  const data = await storageConfig.instance.get(storageConfig.key)
+  rules = data as any;
+  // console.log('rules', rules)
+  // console.log('chrome.alarms.getAll()', await chrome.alarms.getAll())
+
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    if (tabs.length > 0) {
+      currentTab = tabs[0];
+      console.log('Initial active tab:', currentTab);
+    } else {
+      console.log('No active tab found during initialization.');
+    }
+  });
+  await chrome.alarms.clearAll()
 }
 
-setInterval(checkForIdleTabs, 5000);
-
-
-
+main()
 storageConfig.instance.watch({
   [AUTO_CLOSE_TAB_RULES]: (c) => {
     rules = c.newValue;
-    console.log(c.newValue)
   }
 });
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete') {
+    console.log('tab---onUpdated tab')
+    if (tab.active) {
+      currentTab = tab;
+    }
+  }
+});
+chrome.alarms.onAlarm.addListener(closeTab);
 
-export { };
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+  await chrome.alarms.clear(String(activeInfo.tabId));
+  if (currentTab) {
+    await createAlarmForTab(currentTab);
+  }
+  chrome.tabs.get(activeInfo.tabId, function (tab) {
+    currentTab = tab;
+  });
+  // chrome.tabs.query({}, (tabs) => {
+  //   console.log('tabs', tabs.map(i => i.url + i.id))
+  // });
+});
+
+chrome.tabs.onRemoved.addListener(async function (tabId) {
+  await chrome.alarms.clear(String(tabId));
+});
+
--- rule-table.tsx
+++ rule-table.tsx
@@ -9,9 +9,11 @@ import {
   TableRow,
 } from "@/components/ui/table"
 import { useStorage } from "@plasmohq/storage/hook";
+import { useEffect, useState } from "react";
 import { CiEdit } from "react-icons/ci";
 import { IoMdAdd } from "react-icons/io";
 import { MdDeleteForever } from "react-icons/md";
+import { GrClear } from "react-icons/gr";
 
 import { Button } from "~components/ui/button"
 import { defaultValueFunction, storageConfig, useCurrentIdStore, useFormVisibleStore } from "~store";
@@ -37,11 +39,49 @@ const RuleTable = () => {
     setId("")
     setIsOpen(true)
   }
+  const handleClearAlarms = async () => {
+    await chrome.alarms.clearAll();
+    await fetchAlarms()
+  }
+  // 添加一个状态来存储alarms信息
+  const [alarms, setAlarms] = useState([]);
+  const fetchAlarms = async () => {
+    const alarms = await chrome.alarms.getAll();
+    console.log('alarms', alarms)
+    setAlarms(alarms); // 更新状态以存储alarms信息
+  };
+  useEffect(() => {
+    // 当组件挂载时获取所有alarms
+    fetchAlarms();
+  }, []); // 空依赖数组意味着这个effect仅在组件挂载时运行
+
+  // 将Unix时间戳转换为更易读的格式（时分秒）
+  const formatTime = (timestamp) => {
+    const date = new Date(timestamp);
+    return new Intl.DateTimeFormat('en-US', {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false // 24小时制
+    }).format(date);
+  };
+
   return (
     <>
-      <Button variant="outline" size="icon" className="mb-2" onClick={handleAdd}>
+      <Button variant="outline" size="icon" className="mb-2 mr-2" onClick={handleAdd}>
         <IoMdAdd className="h-5 w-5" />
       </Button>
+      <Button variant="outline" size="icon" className="mb-2" onClick={handleClearAlarms}>
+        <GrClear className="h-5 w-5" />
+      </Button>
+      <div>
+        {alarms.map((alarm, index) => (
+          <div key={index} className="p-2 m-2 border rounded shadow">
+            <div>{`Alarm Name: ${alarm.name}`}</div>
+            <div>{`Scheduled Time: ${formatTime(alarm.scheduledTime)}`}</div>
+          </div>
+        ))}
+      </div>
       <Table >
         <TableHeader>
           <TableRow>
